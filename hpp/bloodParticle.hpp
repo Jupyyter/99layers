a4bfae39fda5f1eaa2ab232f639864b3c5befebb
) <raducea.matei2005@gmail.com>
2024-10-25 11:03:34 +0300

    a

--- hpp/bloodParticle.hpp
+++ hpp/bloodParticle.hpp
@@ -1,9 +1,10 @@
 #pragma once
-class bloodParticle:public Sprite ,public CollisionDetector {
+class bloodParticle:public Animation ,public CollisionDetector {
 public:
 bloodParticle(sf::Vector2f position);
 void update(float deltaTime, const sf::Vector2u& windowSize) override;
 private:
     void spawnParticles(sf::Vector2f position);
     void onCollision(Sprite *other) override;
+    void loadSprite();
 };
